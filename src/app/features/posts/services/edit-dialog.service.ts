fd7dcc9f5ffbe55f934c97701a2f7e133b3a7e16
Victor Figueiredo <vcfo081@gmail.com>
2024-07-01 17:36:51 -0300

    fix: dialog component

--- src/app/features/posts/services/edit-dialog.service.ts
+++ src/app/features/posts/services/edit-dialog.service.ts
@@ -1,5 +1,10 @@
 import { Component, inject, Injectable } from "@angular/core";
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 
@@ -7,12 +12,14 @@ import { Observable } from "rxjs";
   selector: "app-edit-dialog",
   template: `
     <h3 mat-dialog-title>Hello User.</h3>
-    <h2 mat-dialog-subtitle>Would you like to edit this info?</h2>
+    <h4 mat-dialog-title>Would you like to edit this info?</h4>
     <div mat-dialog-actions>
       <button mat-button (click)="onYes()">Yes, I'd like to!</button>
       <button mat-button (click)="onNo()">Not yet.</button>
     </div>
   `,
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
 })
 export class EditDialogComponent {
   matDialogRef = inject(MatDialogRef);
