34b49a607b01ecbf15b2ef96de8dd90396189e88
Victor Figueiredo <vcfo081@gmail.com>
2024-07-01 10:07:59 -0300

    chore: folders and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 import { EditPostComponent } from "./features/post/list/list.component";
 import { EditDialogComponent } from "./features/post/edit-dialog/edit-dialog.component";
 import { DataScreenComponent } from "./features/post/edit/edit.component";
-import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
-import { ThanksPageComponent } from "./thanks-page/thanks-page.component";
+import { ThanksPageComponent } from "./features/thanks-page/thanks-page.component";
+import { WelcomePageComponent } from "./features/welcome-page/welcome-page.component";
 
 const routes: Routes = [
   { path: "post-list", component: EditPostComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 
 // Components imports
-import { EditPostComponent } from "./features/post/list/list.component";
 import { EditDialogComponent } from "./features/post/edit-dialog/edit-dialog.component";
 import { DataScreenComponent } from "./features/post/edit/edit.component";
-import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
 
 // Material Design imports below
 import { MatButtonModule } from "@angular/material/button";
@@ -24,7 +22,9 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { ThanksPageComponent } from "./thanks-page/thanks-page.component";
+import { ThanksPageComponent } from "./features/thanks-page/thanks-page.component";
+import { WelcomePageComponent } from "./features/welcome-page/welcome-page.component";
+import { EditPostComponent } from "./features/post/list/list.component";
 
 @NgModule({
   declarations: [
