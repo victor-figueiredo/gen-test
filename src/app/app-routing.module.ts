a59b865e82252529d55c0d726e51fd44f5dcbe10
Victor Figueiredo <vcfo081@gmail.com>
2024-07-01 16:34:27 -0300

    refactor: update routes and add route resolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { EditPostComponent } from "./features/post/list/list.component";
-import { EditDialogComponent } from "./features/post/edit-dialog/edit-dialog.component";
-import { DataScreenComponent } from "./features/post/edit/edit.component";
-import { ThanksPageComponent } from "./features/thanks-page/thanks-page.component";
+import { PostsListComponent } from "./features/posts/list/list.component";
+import { getPost } from "./shared/resolvers/get-post.resolver";
+import { EditPostComponent } from "./features/posts/edit/edit.component";
 import { WelcomePageComponent } from "./features/welcome-page/welcome-page.component";
+import { ThanksPageComponent } from "./features/thanks-page/thanks-page.component";
 
 const routes: Routes = [
-  { path: "post-list", component: EditPostComponent },
-  { path: "edit-dialog", component: EditDialogComponent },
-  { path: "edit-post", component: DataScreenComponent },
-  { path: "welcome-page", component: WelcomePageComponent },
-  { path: "thanks-page", component: ThanksPageComponent },
+  { path: "post-list", component: PostsListComponent },
+  {
+    path: "edit-post/:id",
+    resolve: { post: getPost },
+    component: EditPostComponent,
+  },
+  {
+    path: "welcome-page",
+    component: WelcomePageComponent,
+  },
+  {
+    path: "thanks-page",
+    component: ThanksPageComponent,
+  },
   { path: "", redirectTo: "welcome-page", pathMatch: "full" },
+  { path: "**", redirectTo: "welcome-page", pathMatch: "full" },
 ];
 
 @NgModule({
